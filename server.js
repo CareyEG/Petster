44e80d2ef5307009a7903afa2c0440c15bd387a8
Evan Brecht-Curry <starfoxz13@gmail.com>
2019-06-26 14:12:17 -0700

    adding psql to heroku

--- server.js
+++ server.js
@@ -12,6 +12,19 @@ require('dotenv').config();
 // Database set up
 const client = new pg.Client(process.env.DATABASE_URL)
 client.connect();
+client.query(`SELECT * FROM favorites`)
+  .catch(() => client.query(`CREATE TABLE favorites (
+    id SERIAL PRIMARY KEY,
+    type VARCHAR(255),
+    name VARCHAR(255),
+    age VARCHAR(255),
+    gender VARCHAR(255),
+    size VARCHAR(255),
+    city VARCHAR(255),
+    state VARCHAR(255),
+    description TEXT,
+    photo VARCHAR(255)
+  );`))
 client.on('err', err => console.error(err));
 
 // Application Setup
