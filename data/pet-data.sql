56805a3735d8ba0d948c2f8b913e86a4680bae0d
NaamaBarIlan <barilan.naama@gmail.com>
2019-07-11 12:54:54 -0700

    added petfinder ID to sql file

--- data/pet-data.sql
+++ data/pet-data.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS favorites;
 
 CREATE TABLE favorites (
   id SERIAL PRIMARY KEY,
+  petfinderid VARCHAR(255),
   type VARCHAR(255),
   name VARCHAR(255),
   age VARCHAR(255),
@@ -14,5 +15,5 @@ CREATE TABLE favorites (
   url TEXT
 );
 
-INSERT INTO favorites (type, name, age, gender, size, city, state, description, photo, url)
-VALUES('Cat','Merida','Young','Female','Medium','Orange','CA','Very sweet, she gets along with other dogs', 'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/45078342/1/?bust=1561486424', 'https://www.petfinder.com/dog/spot-120/nj/jersey-city/nj333-petfinder-test-account/?referrer_id=d7e3700b-2e07-11e9-b3f3-0800275f82b1');
+INSERT INTO favorites (petfinderid, type, name, age, gender, size, city, state, description, photo, url)
+VALUES('111','Cat','Merida','Young','Female','Medium','Orange','CA','Very sweet, she gets along with other dogs', 'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/45078342/1/?bust=1561486424', 'https://www.petfinder.com/dog/spot-120/nj/jersey-city/nj333-petfinder-test-account/?referrer_id=d7e3700b-2e07-11e9-b3f3-0800275f82b1');
